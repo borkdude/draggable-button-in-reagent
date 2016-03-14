6f429ea9058ceba036d86bfb57ab28a9863f01cb
Damien Ragoucy <damien@praditus.com>
2016-03-14 12:58:59 +0100

    Make smaller functions

--- src-cljs/drag/main.cljs
+++ src-cljs/drag/main.cljs
@@ -7,60 +7,80 @@
     [goog.events :as events])
   (:import [goog.events EventType]))
 
+
 (enable-console-print!)
 
+
+;; Constants and references
+
 (def black-hole-pos {:x 400 :y 400})
+
 (def draggable (atom {:x 100 :y 100 :alive? true}))
+
+
+;; Utility functions
 
 (defn close? [x y]
   (and (< (Math/abs (- x (:x black-hole-pos))) 50)
        (< (Math/abs (- y (:y black-hole-pos))) 50)))
 
+
 (defn get-client-rect [evt]
   (let [r (.getBoundingClientRect (.-target evt))]
     {:left (.-left r), :top (.-top r)}))
 
+
+;; Event handlers
+
+(defn mouse-move-handler [offset]
+  (fn [evt]
+    (let [x (- (.-clientX evt) (:x offset))
+          y (- (.-clientY evt) (:y offset))]
+      (if (close? x y)
+        (reset! draggable {:alive? false})
+        (reset! draggable {:x      x
+                           :y      y
+                           :alive? true})))))
+
+
+(defn mouse-up-handler [on-move]
+  (fn me [evt]
+    (events/unlisten js/window EventType.MOUSEMOVE
+                     on-move)))
+
+
+(defn mouse-down-handler [e]
+  (let [{:keys [left top]} (get-client-rect e)
+        offset             {:x (- (.-clientX e) left)
+                            :y (- (.-clientY e) top)}
+        on-move            (mouse-move-handler offset)]
+    (events/listen js/window EventType.MOUSEMOVE
+                   on-move)
+    (events/listen js/window EventType.MOUSEUP
+                   (mouse-up-handler on-move))))
+
+
+;; Rea[ct|gent] components
+
 (defn draggable-button []
-  (let [mouse-move-handler
-        (fn [offset]
-          (fn [evt]
-            (let [x (- (.-clientX evt) (:x offset))
-                  y (- (.-clientY evt) (:y offset))]
-              (if (close? x y)
-                (reset! draggable {:alive? false})
-                (reset! draggable {:x x
-                                   :y y
-                                   :alive? true})))))
-        mouse-up-handler
-        (fn [on-move]
-          (fn me [evt]
-            (events/unlisten js/window EventType.MOUSEMOVE
-                             on-move)))]
-    (fn []
-      [:div
-       [:h1 (pr-str @draggable)]
-       [:button.btn.btn-default
-        {:style {:position "absolute"
-                 :left (str (:x black-hole-pos) "px");
-                 :top (str (:y black-hole-pos) "px")
-                 :background "color: black;"}}
-        "Not here please"]
-       (if (:alive? @draggable)
-         [:button.btn.btn-default
-          {:style {:position "absolute"
-                   :left (str (:x @draggable) "px");
-                   :top (str (:y @draggable) "px")}
-           :on-mouse-down (fn [e]
-                            (let [{:keys [left top]} (get-client-rect e)
-                                  offset {:x (- (.-clientX e) left)
-                                          :y (- (.-clientY e) top)}
-                                  on-move (mouse-move-handler offset)]
-                              (events/listen js/window EventType.MOUSEMOVE
-                                             on-move)
-                              (events/listen js/window EventType.MOUSEUP
-                                             (mouse-up-handler on-move)))
-                            )}
-          "Drag me"])])))
+  [:div
+   [:h1 (pr-str @draggable)]
+   [:button.btn.btn-default
+    {:style {:position   "absolute"
+             :left       (str (:x black-hole-pos) "px") ;
+             :top        (str (:y black-hole-pos) "px")
+             :background "color: black;"}}
+    "Not here please"]
+   (if (:alive? @draggable)
+     [:button.btn.btn-default
+      {:style         {:position "absolute"
+                       :left     (str (:x @draggable) "px") ;
+                       :top      (str (:y @draggable) "px")}
+       :on-mouse-down mouse-down-handler}
+      "Drag me"])])
+
+
+;; Initialization
 
 (reagent/render-component [draggable-button]
                           (js/document.getElementById "app"))
